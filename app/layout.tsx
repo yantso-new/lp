90b2f9969e2f961de2c530b5d389ea705da1bcfd
yantso-new <hello@yantso.com>
2025-12-22 08:22:46 +0000

    removed posthog added vercel analatycs

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SmoothScroll } from "@/components/ui/smooth-scroll";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         <SmoothScroll />
         {children}
+        <Analytics />
       </body>
     </html>
   );
